61040c7bbaefdbcb5a9add701c9fa2c34919fbbd
Shanna M. Sullivan <shannasullivanold@Shannas-MacBook-Pro.local>
2015-07-10 01:33:05 -0700

    add skills diagram to skills section. make skills section toggle. make page reload on window resize to skills diagram is responsive.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -339,6 +339,60 @@ function inName() {
 // append map to resume
 $("#mapDiv").append(googleMap);
 
+
+// skills graphic - radial reingold-tilford tree from mbostock
+// links to d3 and data from flare.json
+// code from mbostock (followed d3 link from course video)
+var diameter = $(window).width(); // changed from 960px to make responsive
+
+var tree = d3.layout.tree()
+		.size([360, diameter / 2 - 120])
+		.separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+
+var diagonal = d3.svg.diagonal.radial()
+		.projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+
+var svg = d3.select("#skillDiagram").append("svg") // changed body to #skillDiagram
+		.attr("width", diameter)
+		.attr("height", diameter + 100) // changed - 150 to + 100 to make text fit on page
+	.append("g")
+		.attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
+d3.json("flare.json", function(error, root) {
+	if(error){throw(error)};
+
+	var nodes = tree.nodes(root),
+			links = tree.links(nodes);
+
+	var link = svg.selectAll(".link")
+			.data(links)
+		.enter().append("path")
+			.attr("class", "link")
+			.attr("d", diagonal);
+
+	var node = svg.selectAll(".node")
+			.data(nodes)
+		.enter().append("g")
+			.attr("class", "node")
+			.attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+
+	node.append("circle")
+			.attr("r", 4.5);
+
+	node.append("text")
+			.attr("dy", ".31em")
+			.attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+			.attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+			.text(function(d) { return d.name; });
+});
+
+d3.select(self.frameElement).style("height", diameter - 150 + "px");
+
+// make browser refresh if window is resized so skills diagram will update based on window size
+$(window).resize(function(){
+	location.reload();
+});
+
 // toggle each resume section
 $(document).ready(function(){
 	/*
@@ -351,7 +405,7 @@ $(document).ready(function(){
 	*/
 
 	// hide collapsible content to start
-	$(".work-entry, .project-entry, .education-entry, #online-education").hide();
+	$(".work-entry, .project-entry, .education-entry, #online-education, svg").hide();
 	
 	// when toggle class is clicked, run this function
 	$(".toggle").click(function() {
@@ -360,9 +414,6 @@ $(document).ready(function(){
 	});
 
 });
-
-
-
 
 
 
